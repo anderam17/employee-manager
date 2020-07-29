5fb25736539a358b32bd2fb65da7d324d9f5cd12
Allana Anderson <allana.anderson17@gmail.com>
2020-07-29 09:54:29 -0600

    added functions to view departments, roles, and employees. Need to add seed data to test them.

--- employee.js
+++ employee.js
@@ -10,38 +10,72 @@ const connection = mysql.createConnection({
 });
 
 function start() {
-  inquirer.prompt({
-    name: "action",
-    type: "list",
-    message: "What would you like to do?",
-    choices: [
-      "Add Department",
-      "Add Role",
-      "Add Employee",
-      "View Departments",
-      "View Roles",
-      "View Employees",
-      "Update Employee Roles",
-    ],
-  }).then((ans) => {
+  inquirer
+    .prompt({
+      name: "action",
+      type: "list",
+      message: "What would you like to do?",
+      choices: [
+        "Add Department",
+        "Add Role",
+        "Add Employee",
+        "View Departments",
+        "View Roles",
+        "View Employees",
+        "Update Employee Roles",
+      ],
+    })
+    .then((ans) => {
       console.log(ans);
-    switch(ans.action){
+      switch (ans.action) {
+        case "View All Departments":
+          viewDepartments();
+          break;
+        case "View All Roles":
+          viewRoles();
+          break;
+        case "View All Employees":
+          viewEmployees();
+          break;
         case "Add Department":
         case "Add Role":
         case "Add Employee":
-        case "View Departments":
-        case "View Roles":
-        case "View Employees":
         case "Update Employee Roles":
-            console.log("We did it!")
-            connection.end();
-            break;
+          console.log("We did it!");
+          connection.end();
+          break;
         default:
-            console.log("U suck.")
-            connection.end();
-    }
-  })
+          console.log("U suck.");
+          connection.end();
+      }
+    });
 }
+
+function viewDepartments() {
+  connection.query("SELECT * FROM departments", (err, data) => {
+    if (err) throw err;
+    console.log(data);
+    start();
+  });
+}
+
+function viewRoles() {
+  connection.query("SELECT * FROM roles", (err, data) => {
+    if (err) throw err;
+    console.log(data);
+    start();
+  });
+}
+
+function viewEmployees() {
+  connection.query("SELECT * FROM employees", (err, data) => {
+    if (err) throw err;
+    console.log(data);
+    start();
+  });
+}
+//! NEED TO ADD SEED DATA TO TEST THESE
+//! ADD TABLE THING SARAH SENT OUT IN SLACK
 
 connection.connect((err) => {
   if (err) throw err;
