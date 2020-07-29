f41ebd69c2c54e8bb263b0dd50772643d7c6f27b
Allana Anderson <allana.anderson17@gmail.com>
2020-07-28 20:04:30 -0600

    created json file, git ignore, and beginning inquirer propmt with minimal code after response to test basic functionality.

--- employee.js
+++ employee.js
@@ -2,16 +2,49 @@ const mysql = require("mysql");
 const inquirer = require("inquirer");
 
 const connection = mysql.createConnection({
-    host: "localhost",
-    port: 8889,
-    user: "root",
-    password: "root",
-    database: "employees"
+  host: "localhost",
+  port: 8889,
+  user: "root",
+  password: "root",
+  database: "company",
 });
 
+function start() {
+  inquirer.prompt({
+    name: "action",
+    type: "list",
+    message: "What would you like to do?",
+    choices: [
+      "Add Department",
+      "Add Role",
+      "Add Employee",
+      "View Departments",
+      "View Roles",
+      "View Employees",
+      "Update Employee Roles",
+    ],
+  }).then((ans) => {
+      console.log(ans);
+    switch(ans.action){
+        case "Add Department":
+        case "Add Role":
+        case "Add Employee":
+        case "View Departments":
+        case "View Roles":
+        case "View Employees":
+        case "Update Employee Roles":
+            console.log("We did it!")
+            connection.end();
+            break;
+        default:
+            console.log("U suck.")
+            connection.end();
+    }
+  })
+}
 
 connection.connect((err) => {
-    if (err) throw err;
-    console.log(`Connected as thread id: ${connection.threadId}`);
-    start();
-  });
+  if (err) throw err;
+  console.log(`Connected as thread id: ${connection.threadId}`);
+  start();
+});
