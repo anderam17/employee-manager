f8b64e2d9392b0be6ddf3cd89df85c5c13ae66cd
Allana Anderson <allana.anderson17@gmail.com>
2020-07-29 19:55:37 -0600

    added data to the schema to test the view functions and they seem to work well. Need to format the output. Also created shells for the add functions.

--- employee.js
+++ employee.js
@@ -16,14 +16,15 @@ function start() {
       type: "list",
       message: "What would you like to do?",
       choices: [
+        "View All Departments",
+        "View All Roles",
+        "View All Employees",
         "Add Department",
         "Add Role",
         "Add Employee",
-        "View Departments",
-        "View Roles",
-        "View Employees",
         "Update Employee Roles",
-      ],
+        "Exit"
+      ]
     })
     .then((ans) => {
       console.log(ans);
--- seeds.sql
+++ seeds.sql
@@ -1,1 +1,11 @@
+INSERT INTO departments(name)
+VALUES ("engineering"),("sales"),("finance"),("legal");
+
+INSERT INTO roles(title, salary, department_id)
+VALUES ("engineer", 70000, 10),("lead engineer", 90000, 11),("lawyer", 70000, 20),("lead lawyer", 90000, 21), ("salesperson", 60000, 30), ("lead salesperson", 70000, 31), ("financial advisor", 70000, 40) ("lead financial advisor", 90000, 41);
+
+INSERT INTO employees(first_name, last_name, role_id, manager_id)
+VALUES ("Allana", "Anderson", 10, 2), ("Daniel", "Lee", 11, 1), ("Katlin", "Fletcher", 20, 3), ("KC", "Katalbas", 21, 4), ("Betsy", "Shuttleworth", 30, 5), ("Maia", "Holmes", 31, 6), ("Anthony", "Cortes", 40, 7), ("Willie", "Pointsalot", 41, 8);
+
+
 SELECT * FROM company;
